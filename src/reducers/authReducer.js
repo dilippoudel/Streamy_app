d20eeecaa95aeaa72ee45ca1ba62e3781c3942c8
Dilip Poudel <dilippoudel578@gmail.com>
2019-01-28 11:04:39 +0200

    REST Convention-APp
    This app is practice with REST convention for backend.

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,15 @@
+const INITIAL_STATE = {
+    isSignedIn: null,
+    userId: null
+}
+export default (state = INITIAL_STATE, action)=> {
+    if(action.type==='SIGN_IN'){
+        return {...state, isSignedIn: true, userId: action.payload}
+    }
+    if(action.type === 'SIGN_OUT'){
+        return {...state, isSignedIn:false, userId: null}
+    }
+    else {
+        return state;
+    }
+}
